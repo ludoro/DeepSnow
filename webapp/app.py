b41d30ad3189f6e6328ddeecbc279821521d2c0d
agemcipe <jonathanhaas@arcor.de>
2018-09-22 22:02:15 +0200

    polygon from prediction image working

--- webapp/app.py
+++ webapp/app.py
@@ -1,37 +1,27 @@
-import flask
-from flask import Flask, render_template, request
-from model import *
-from PIL import Image
 import io
+import numpy as np
+
+import flask
+# from model import *
+from PIL import Image
+from flask import Flask, render_template
+
+# polygon rendering
+from open_cv_processing import *
 
 #from flask_restful import Resource, Api, reqparse, abort
 #from opencv import open_cv_processing
 
-from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
 app = Flask(__name__)
 
-#api = Api(app)
-# api.add_resource(ProcessImage, '/process_image')
-# class ProcessImage(Resource):
-#     def get(self):
-#         return None
-#     def post(self):
-#         args = parser.parse_args()
-#         image = args["image"]
-#         binary_image_output = run_model(image)
-#         open_cv_processing(binary_image_output)
-#         return image_with_contour.jpg
 
-
-
-def load_model():
-    # load the pre-trained Keras model (here we are using a model
-    # pre-trained on ImageNet and provided by Keras, but you can
-    # substitute in your own networks just as easily)
-    global model
-    model = unet()
-    model.load_weights("unet_membrane.hdf5")
+#def load_model():
+#    # load the pre-trained Keras model (here we are using a model
+#    # pre-trained on ImageNet and provided by Keras, but you can
+#    # substitute in your own networks just as easily)
+#    global model
+#    model = unet()
+#    model.load_weights("unet_membrane.hdf5")
 
 
 @app.route("/api/prediction", methods=['POST'])
@@ -45,17 +35,22 @@ def image_prediction():
         if image.mode != "L":
             image = image.convert("L")
         # resize the input image and preprocess it
+
         #image = image.resize((512,512))
 
-        preds = model.predict(image)
+        # preds = model.predict(image)
 
-        img = Image()
-        img.getdata(preds)
-        img.show()
+        # img = Image()
+        # img.getdata(preds)
+        #image.show()
+
+        image_cv = np.array(image)
+        poly_json, area = open_cv_processing(image_cv)
         #data["predictions"] = []
 
+        return poly_json, str(area)
 
-    return
+    return 'No Image given!'
 
 @app.route("/", methods=['GET'])
 def home():
@@ -64,5 +59,5 @@ def home():
 
 
 if __name__ == "__main__":
-    load_model()
+    # load_model()
     app.run(debug=True)
